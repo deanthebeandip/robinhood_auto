cf9b339c2c0fda0836c4aaf60615bb7b868775eb
Huang <Dean.Huang@graphicpkg.com>
2022-11-26 10:47:39 -0600

    Added files to repo

---
+++ rbnhd_auto.py
@@ -0,0 +1,49 @@
+from re import L
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import os
+import shutil
+
+import datetime
+import seaborn as sns
+import matplotlib.pyplot as plt
+# from matplotlib import dates
+import matplotlib.dates as mdates
+
+from datetime import timedelta
+import numpy as np
+import plotly.graph_objects as go
+
+from pptx import Presentation
+from pptx.enum.text import PP_ALIGN
+from pptx.dml.color import RGBColor
+from pptx.enum.dml import MSO_THEME_COLOR
+from pptx.util import Pt
+from pptx.util import Inches
+
+from tqdm import tqdm
+
+
+
+# long_dir = r'C:\Users\dean.huang\main\projects\HeidelBergTimeTracking\MR Time Improvement Dean Edit'
+long_dir = r'C:\Users\dean.huang\main\reallife\spending\robinhood_auto'
+
+
+
+
+def main():
+    print(long_dir)
+
+
+    # Stock Notes: 
+    # Should I keep track of gold, FED interest rate, inflation, etc?
+    # Keeping track of Stocks is already good enough?
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
